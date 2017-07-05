fd5e5f4ff94e8b913316fd05d95fe34443825e0d
Mognar <philipbgorman@gmail.com>
2017-07-05 10:42:07 +0100

    Add files via upload

---
+++ Untitled0 (1).py
@@ -0,0 +1,139 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import requests
+
+
+# In[2]:
+
+import time
+
+
+# In[3]:
+
+from time import sleep
+
+
+# In[4]:
+
+get_ipython().system(u'pip install tweepy')
+
+
+# In[5]:
+
+import tweepy
+
+
+# In[6]:
+
+consumer_key = 'P1ruR922GVcRPxmQo9j1yRQdA'
+consumer_secret = 'Lktnuy1oGQ0SO0RIRMNt5RIukSMi6VVaE6e3Pn8XogfOPZLXu2'
+access_token = '854276747096973313-9TWpJFZ63MJBDNyCjck7utSgDwsFlPI'
+access_token_secret = 'MP1prKbW8BjT4Oa37oVbPenTww3jQkKXYHXBh7tuKhQH7'
+
+
+# In[7]:
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+# In[8]:
+
+get_ipython().system(u'pip install beautifulsoup4')
+
+
+# In[9]:
+
+from bs4 import BeautifulSoup
+
+
+# In[10]:
+
+import http.cookiejar
+
+
+# In[11]:
+
+URL = 'http://parliamentlive.tv/Commons'
+#URL = 'http://www.senedd.tv/Meeting/Live'
+#URL = 'http://hansard.parliament.uk/search/MemberContributions?memberId=1522&type=Spoken'
+#jar = http.cookiejar.CookieJar()
+#req = requests.get(URL, cookies=jar)
+#req = requests.get(URL, cookies=jar)
+#req = requests.get(URL)
+
+
+# In[12]:
+
+#soup = BeautifulSoup(req.text, "lxml")
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, "lxml")
+eventurl = soup.find("div", {"class":"carousel-caption row-no-padding"})
+urlnew = eventurl.find("a", href=True)
+fullurl ='http://parliamentlive.tv' + urlnew['href']
+print(fullurl)
+
+
+# In[ ]:
+
+#textout = soup.find("div", "col-md-10 nopadding")
+#textout.h4.unwrap()
+
+
+# In[ ]:
+
+while True: 
+    from datetime import datetime
+    timenow= datetime.now()
+    jar = http.cookiejar.CookieJar()
+    req = requests.get(fullurl, cookies=jar)
+    req = requests.get(fullurl, cookies=jar)
+    soup = BeautifulSoup(req.text, "lxml")
+    textout = soup.find("div", "col-md-10 nopadding")
+    textime = soup.find("span","time-code")
+    #textout = soup.find_all("li")
+    #textout.h4.unwrap()
+    textin = str(textout.text)
+    textimes = str(textime.text)
+    if "Division" in textin:
+        #from datetime import datetime
+        #timenow= str(datetime.now())
+        try:
+            api.update_status("Division, started:" + textimes)
+        except tweepy.TweepError as e:
+            pass 
+        print(textin + textimes)
+        sleep(60)
+    else: 
+        time.sleep(20)     
+    #textin = str(textout)
+    #print (textin)
+    #from datetime import datetime
+    #timenow= str(datetime.now())
+    #print(textin + " " + timenow)
+    #time.sleep(30)
+
+
+# In[ ]:
+
+#from datetime import datetime
+#timenow= str(datetime.now())
+#print(textin + timenow)
+#time.sleep(30)
+
+
+# In[ ]:
+
+#api.update_status("No. of contribs:" + textin + timenow)
+#print(textin + timenow)
+#time.sleep(30)
+
+
+# In[ ]:
+
+
+
